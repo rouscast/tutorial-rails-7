70a6ec3936aa5886baad4d2e790ba1fbac01f2ac
rouscast <rociocy@gmail.com>
2023-09-07 01:55:44 -0300

    edition and delete products

--- app/app/controllers/products_controller.rb
+++ app/app/controllers/products_controller.rb
@@ -20,6 +20,27 @@ class ProductsController < ApplicationController
         end
     end
     
+    def edit
+        @product= Product.find(params[:id])
+    end
+
+    def update
+        @product= Product.find(params[:id])
+        if @product.update(product_params)
+            redirect_to products_path, notice: 'Tu producto se ha modificado correctamente'
+        else
+          render :new, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+        @product= Product.find(params[:id])
+        @product.destroy
+        redirect_to products_path, notice: 'Tu producto se ha eliminado correctamente', status: :see_other
+      
+
+    end
+
     private
 
     def product_params
